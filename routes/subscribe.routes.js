6e21b98b60522948ec1932571a0c697a1cc9736b
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-27 08:10:50 +0100

    only save non empty frecasts

--- routes/subscribe.routes.js
+++ routes/subscribe.routes.js
@@ -16,11 +16,13 @@ async function fetchAndSaveMultipleRegions(regions) {
           const url = `https://us-central1-engaged-card-410714.cloudfunctions.net/new-function`;
           const response = await axios.post(url, { region });  // Pass the region in the request body
           // only create new when not empty
-
+          console.log(response.data.forecast_result.length)
+          if (response.data.forecast_result.length !== 0) {
           // here comes the code to check if dat ais not empty
-          const newData = new RegionData({ region, data: response.data });
-          await newData.save();
-          console.log('Data saved successfully for region:', region);
+            const newData = new RegionData({ region, data: response.data });
+            await newData.save();
+            console.log('Data saved successfully for region:', region);
+          }
       }
       console.log('Data fetching and saving completed for all requested regions.');
   } catch (error) {
