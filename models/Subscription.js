226b662509aafaedff503df075f992cfc184617d
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-03-01 11:21:10 +0100

    editing Subscription model

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,15 +1,16 @@
 // Example Subscriber model (adjust fields as necessary)
-const mongoose = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const subscriptionSchema = new mongoose.Schema({
+
+const subscriptionSchema = new Schema({
   email: { type: String, required: true },
   region: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
 
-const Subscription = mongoose.model('Subscription', subscriptionSchema); // Correct the casing of regiondataSchema
+const Subscription = model('Subscription', subscriptionSchema); // Correct the casing of regiondataSchema
 
-module.exports = mongoose.model('Subscription', subscriptionSchema);
+module.exports = Subscription;
 
 
